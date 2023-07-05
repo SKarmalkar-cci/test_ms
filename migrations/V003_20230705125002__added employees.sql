0045e0d4dc197bc9fd43d75c97a820bce4ea7064
CREATIVECAPSULE\Sangeeta <sangeeta.karmalkar@creativecapsule.com>
2023-07-05 18:21:26 +0530

    added employees

---
+++ migrations/V003_20230705125002__added employees.sql
@@ -0,0 +1,17 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Creating [dbo].[employees]'
+GO
+CREATE TABLE [dbo].[employees]
+(
+[emp_no] [int] NOT NULL,
+[birth_date] [date] NOT NULL,
+[first_name] [varchar] (14) NOT NULL,
+[last_name] [varchar] (16) NOT NULL,
+[hire_date] [date] NOT NULL,
+[middle_name] [varchar] (45) NULL
+)
+GO
+
