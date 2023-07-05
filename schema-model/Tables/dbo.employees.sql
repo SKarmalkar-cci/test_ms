2c42c718def6383802b3400429057728dc7169c4
CREATIVECAPSULE\Sangeeta <sangeeta.karmalkar@creativecapsule.com>
2023-07-05 18:18:14 +0530

    added employees

---
+++ schema-model/Tables/dbo.employees.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [dbo].[employees]
+(
+[emp_no] [int] NOT NULL,
+[birth_date] [date] NOT NULL,
+[first_name] [varchar] (14) NOT NULL,
+[last_name] [varchar] (16) NOT NULL,
+[hire_date] [date] NOT NULL,
+[middle_name] [varchar] (45) NULL
+)
+GO
