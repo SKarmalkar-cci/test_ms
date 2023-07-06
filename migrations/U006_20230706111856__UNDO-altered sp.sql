ad93a6fc1148ed87cb40d0edfbde86108fa82faa
CREATIVECAPSULE\Sangeeta <sangeeta.karmalkar@creativecapsule.com>
2023-07-06 16:50:57 +0530

    altered sp

---
+++ migrations/U006_20230706111856__UNDO-altered sp.sql
@@ -0,0 +1,22 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Altering [dbo].[usp_get_employees]'
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER PROCEDURE [dbo].[usp_get_employees]
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+select * from employees
+END
+GO
+
