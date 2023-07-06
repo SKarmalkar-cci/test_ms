bcf020b141178a3a669f31ca1dae454353f732eb
CREATIVECAPSULE\Sangeeta <sangeeta.karmalkar@creativecapsule.com>
2023-07-06 13:08:37 +0530

    added views and sps

---
+++ migrations/U005_20230706073706__UNDO-Sangeeta.sql
@@ -0,0 +1,13 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Dropping [dbo].[employeedetails]'
+GO
+DROP VIEW [dbo].[employeedetails]
+GO
+PRINT N'Dropping [dbo].[usp_get_employees]'
+GO
+DROP PROCEDURE [dbo].[usp_get_employees]
+GO
+
